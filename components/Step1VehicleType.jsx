151108d0788be3f601fda283566fef6a58285b13
michaljanko <michaljanko@outlook.sk>
2025-06-23 14:20:02 +0200

    Delete components directory

--- components/Step1VehicleType.jsx
+++
@@ -1,1 +0,0 @@
-export default function Step1({ onNext }) { return (<div><label>Typ auta:<select><option>mestské</option><option>kompaktné</option></select><button onClick={onNext}>Ďalej</button></div>); }
